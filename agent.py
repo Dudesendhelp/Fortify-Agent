8bc1d9c9dd04a38781cda634eb57ce6ddcba1b97
Dudesendhelp <zoomassembly@gmail.com>
2025-12-11 11:09:54 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,7 +1,23 @@
-from image_cap import capture_image
+import cv2
+import time
 import requests
+import os
 
-filename=capture_image()
+if not os.path.exists("uploads"):
+    os.makedirs("uploads")
+
+def capture_image():
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+
+    if ret:
+        filename = time.strftime("capture_%Y-%m-%d_%H-%M-%S.jpg")
+        filepath = os.path.join("uploads", filename)
+        cv2.imwrite(filepath, frame)
+        cap.release()
+        return filepath  
+
+filename = capture_image()
 
 if filename is None:
     print("Failed to capture image")
